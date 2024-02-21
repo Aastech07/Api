17da6a58ebc84f281f05f928f30b19c31c046ca3
Aasmohammad <ansariaasmohammad17@gmail.com>
2024-02-21 15:10:24 +0530

    chenge file

--- routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cartController = require("../controller/cartController");
-
-router.post("/addToCart", cartController.addToCart);
-
-router.get("/getCartItems/:userId", cartController.getCartItems);
-
-module.exports = router;
